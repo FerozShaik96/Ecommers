2d699c1aaf31ba11079a056b836b329c23d62344
FerozShaik96 <ferozshaik1997@gmail.com>
2023-06-19 20:06:50 +0530

    Adding the Form to enter the New movie details

---
+++ src/Home/HomeForm/HomeForm.js
@@ -0,0 +1,58 @@
+import React, { useRef } from "react";
+import { Container, Row, Col, Form, Button } from "react-bootstrap";
+
+const HomeForm = () => {
+  const NameRef = useRef();
+  const DirectorRef = useRef();
+  const DateRef = useRef();
+  const submitHandler = (event) => {
+    event.preventDefault();
+    const MovieName = NameRef.current.value;
+    const DirectorName = DirectorRef.current.value;
+    const DateNumber = DateRef.current.value;
+    const userData = {
+      Movie: MovieName,
+      Director: DirectorName,
+      Date: DateNumber,
+      id: Math.random().toString(),
+    };
+    console.log(userData);
+  };
+  return (
+    <Container>
+      <Row>
+        <Col className="bg-light d-flex justify-content-center mt-4">
+          <Form onSubmit={submitHandler}>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label>Movie Name </Form.Label>
+              <Form.Control
+                ref={NameRef}
+                style={{ width: "500px" }}
+                type="text"
+                placeholder="Movie Name"
+              />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Label>Movie Director</Form.Label>
+              <Form.Control
+                type="text"
+                ref={DirectorRef}
+                placeholder="Director"
+              />
+            </Form.Group>
+            <Form.Group className="mb-4" controlId="formBasicPassword">
+              <Form.Label>Release Date</Form.Label>
+              <Form.Control type="date" ref={DateRef} />
+            </Form.Group>
+            <div className="d-flex justify-content-center">
+              <Button variant="info" type="submit">
+                Submit
+              </Button>
+            </div>
+          </Form>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+export default HomeForm;
