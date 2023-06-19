4346efcca0efe68901220527e0ace5610d3f93d2
FerozShaik96 <ferozshaik1997@gmail.com>
2023-06-19 14:02:39 +0530

    Addded Error Message

--- src/Home/HomeContent/HomeContent.js
+++ src/Home/HomeContent/HomeContent.js
@@ -6,42 +6,51 @@ const HomeContent = () => {
   const [show, setUnShow] = useState(true);
   const [Movies, setMovies] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
   const fetchData = async () => {
     setUnShow(false);
     setIsLoading(true);
-    const Data = await fetch("https://swapi.dev/api/films");
-    let response = await Data.json();
-    const transferedData = response.results.map((item) => {
-      return {
-        id: item.episode_id,
-        date: item.release_date,
-        title: item.title,
-        director: item.director,
-      };
-    });
-    setMovies(transferedData);
+    setError(null);
+    try {
+      const Data = await fetch("https://swapi.dev/api/film/ ");
+      if (Data.status !== 200) {
+        throw new Error("Something Went Wrong Please Try again later");
+      }
+      let response = await Data.json();
+      const transferedData = response.results.map((item) => {
+        return {
+          id: item.episode_id,
+          date: item.release_date,
+          title: item.title,
+          director: item.director,
+        };
+      });
+      setMovies(transferedData);
+    } catch (error) {
+      setError(error.message);
+    }
     setIsLoading(false);
   };
-  console.log(Movies);
   return (
     <React.Fragment>
       <Container>
         <ul className="my-5">
-          {Movies.map((item) => (
-            // <div>
-            <li
-              key={item.id}
-              className="d-flex justify-content-between pt-4   border-bottom"
-            >
-              <span>{item.date} </span>
-              <span>{item.director} </span>
-              <span>{item.title}</span>
-              <span>
-                <Button className="mb-3">Buy Tickets</Button>
-              </span>
-            </li>
-            // </div>
-          ))}
+          {!isLoading &&
+            Movies.map((item) => (
+              // <div>
+              <li
+                key={item.id}
+                className="d-flex justify-content-between pt-4   border-bottom"
+              >
+                <span>{item.date} </span>
+                <span>{item.director} </span>
+                <span>{item.title}</span>
+                <span>
+                  <Button className="mb-3">Buy Tickets</Button>
+                </span>
+              </li>
+              // </div>
+            ))}
         </ul>
       </Container>
       {show && (
@@ -51,6 +60,7 @@ const HomeContent = () => {
           </Button>
         </div>
       )}
+      {!isLoading && error && <p>{error}</p>}
       {isLoading && <Loading />}
     </React.Fragment>
   );
