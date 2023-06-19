33ce20da72773450ad0c977b4e981aaa5d8e03b5
FerozShaik96 <ferozshaik1997@gmail.com>
2023-06-19 20:07:19 +0530

    Rendering the form

--- src/Home/Homereneder/HomePage.js
+++ src/Home/Homereneder/HomePage.js
@@ -3,11 +3,13 @@ import Footer from "../../UI/Footer/Footer";
 import HomeNavBar from "../HomeNavBar/HomeNavBar";
 import HomeHeader from "../HomeHeader/Header";
 import HomeContent from "../HomeContent/HomeContent";
+import HomeForm from "../HomeForm/HomeForm";
 const HomePage = () => {
   return (
     <React.Fragment>
       <HomeNavBar />
       <HomeHeader />
+      <HomeForm />
       <HomeContent />
       <Footer />
     </React.Fragment>
