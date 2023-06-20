0a0c5035359b21a0b3bf56723110fa68b9ca8456
FerozShaik96 <ferozshaik1997@gmail.com>
2023-06-20 23:41:16 +0530

    Creted router for Contact Page & added in app.js

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import HomePage from "./Home/Homereneder/HomePage";
 import Storepage from "./components/StoreDisplay/StorePage";
 import About from "./About/Aboutrender/About";
+import ContactPage from "./Pages/ContactPage";
 
 const router = createBrowserRouter([
   { path: "/", element: <HomePage /> },
   { path: "/home", element: <HomePage /> },
   { path: "/store", element: <Storepage /> },
   { path: "/about", element: <About /> },
+  { path: "/contact", element: <ContactPage /> },
 ]);
 
 function App() {
