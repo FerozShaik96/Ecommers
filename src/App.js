e4fe5cc97de24528896737c72a487d1c3423a024
FerozShaik96 <ferozshaik1997@gmail.com>
2023-06-21 12:48:20 +0530

    Implemented Layout & Nested Routes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 import React from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import HomePage from "./Home/Homereneder/HomePage";
-import Storepage from "./components/StoreDisplay/StorePage";
+import HomePage from "./Pages/Homereneder/HomePage";
+import Storepage from "./Pages/StoreDisplay/StorePage";
 import About from "./About/Aboutrender/About";
+import Root from "./Pages/Root/Root";
 import ContactPage from "./Pages/ContactPage";
 
 const router = createBrowserRouter([
-  { path: "/", element: <HomePage /> },
-  { path: "/home", element: <HomePage /> },
+  {
+    path: "/",
+    element: <Root />,
+    children: [
+      { path: "/", element: <HomePage /> },
+      { path: "/home", element: <HomePage /> },
+      { path: "/about", element: <About /> },
+      { path: "/contact", element: <ContactPage /> },
+    ],
+  },
   { path: "/store", element: <Storepage /> },
-  { path: "/about", element: <About /> },
-  { path: "/contact", element: <ContactPage /> },
 ]);
 
 function App() {
