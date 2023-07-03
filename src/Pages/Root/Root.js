913b79668c2f2ee139af6910b51d24d0f54ccb4a
FerozShaik96 <ferozshaik1997@gmail.com>
2023-07-03 15:24:48 +0530

    removied CardProvider

--- src/Pages/Root/Root.js
+++ src/Pages/Root/Root.js
@@ -1,15 +1,16 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import CartProvider from "../../Store/CartProvider";
 import NavBarUi from "../../UI/NavBar/NavBarUi";
 import Footer from "../../UI/Footer/Footer";
 
 const Root = () => {
   return (
-    <>
+    <CartProvider>
       <NavBarUi />
       <Outlet />
       <Footer />
-    </>
+    </CartProvider>
   );
 };
 export default Root;
