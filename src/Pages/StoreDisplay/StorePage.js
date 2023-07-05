88ac72ce5abd3dfacc3f56007737461c8814093b
FerozShaik96 <ferozshaik1997@gmail.com>
2023-07-05 23:24:22 +0530

    fetch the data from a api using loder and defer

--- src/Pages/StoreDisplay/StorePage.js
+++ src/Pages/StoreDisplay/StorePage.js
@@ -1,23 +1,33 @@
-import React from "react";
-import { useLoaderData } from "react-router-dom";
+import React, { Suspense } from "react";
+import { Await, defer, json, useRouteLoaderData } from "react-router-dom";
 import Header from "../../components/Header/Header";
 import Display from "../../components/Display/Display";
 const Storepage = () => {
-  const product = useLoaderData();
+  const { products } = useRouteLoaderData("product-details");
+
   return (
-    <>
-      {/* <NavBar /> */}
-      <Header />
-      <Display />
-      {/* <Footer /> */}
-    </>
+    <Suspense fallback={<p style={{ textAlign: "center" }}>Loading...</p>}>
+      <Await resolve={products}>
+        {(loaderProducts) => {
+          return <Display products={loaderProducts} />;
+        }}
+      </Await>
+    </Suspense>
   );
 };
 export default Storepage;
 
-export async function Loader() {
+async function LoaderData() {
   const data = await fetch("https://fakestoreapi.com/products?limit=5");
-  const res = data.json();
-  console.log(res);
-  return res;
+  if (!data.ok) {
+    throw json({ message: "Could not able to fetch Data" });
+  }
+  const response = await data.json();
+  console.log(response);
+  return response;
+}
+export function Loader() {
+  return defer({
+    products: LoaderData(),
+  });
 }
--- src/components/Display/Display.js
+++ src/components/Display/Display.js
@@ -1,46 +1,18 @@
 import React from "react";
 import Content from "../Content/Content";
-const productsArr = [
-  {
-    id: 1,
-    title: "Colors",
-    price: 100,
-    imageUrl: "https://prasadyash2411.github.io/ecom-website/img/Album%201.png",
-  },
-
-  {
-    id: 2,
-    title: "Black and white Colors",
-    price: 50,
-    imageUrl: "https://prasadyash2411.github.io/ecom-website/img/Album%202.png",
-  },
-
-  {
-    id: 3,
-    title: "Yellow and Black Colors",
-    price: 70,
-    imageUrl: "https://prasadyash2411.github.io/ecom-website/img/Album%203.png",
-  },
-
-  {
-    id: 4,
-    title: "Blue Color",
-    price: 100,
-    imageUrl: "https://prasadyash2411.github.io/ecom-website/img/Album%204.png",
-  },
-];
-function Display() {
+function Display(props) {
+  console.log(props.products);
   return (
     <React.Fragment>
       <h2 className="text-center my-4">Music </h2>
       <div className="container my-4">
         <div className="row justify-content-center align-items-center ">
-          {productsArr.map((item, index) => {
+          {props.products.map((item, index) => {
             return (
               <Content
                 key={item.id}
                 id={item.id}
-                url={item.imageUrl}
+                url={item.image}
                 title={item.title}
                 price={item.price}
               />
