55eada05039869c84bfab70e55bc3a5020a9649e
FerozShaik96 <ferozshaik1997@gmail.com>
2023-07-11 00:06:19 +0530

    Implemented the Protected Routes using Private Routes

--- src/App.js
+++ src/App.js
@@ -11,46 +11,49 @@ import ContactPage from "./Pages/ContactPage";
 import SignIn from "./Login Pages/SignIn";
 import SignUp from "./Login Pages/SignUp";
 import ForgotPassword from "./Login Pages/ForgotPassword";
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Root />,
-    children: [
-      { index: true, element: <HomePage /> },
-      { path: "home", element: <HomePage /> },
-      { path: "about", element: <AboutPage /> },
-      { path: "contact", element: <ContactPage /> },
-      {
-        path: "signin",
-        element: <SignIn />,
-      },
-      { path: "forgotPassword", element: <ForgotPassword /> },
-      {
-        path: "signup",
-        element: <SignUp />,
-      },
-      {
-        path: "store",
-        id: "product-details",
-        loader: displayLoader,
-        children: [
-          { index: true, element: <Storepage /> },
-          { path: ":id", element: <ProductDisplay /> },
-        ],
-      },
-    ],
-  },
-  // {
-  //   path: "/store",
-  //   element: <Storepage />,
-  //   children: [
-  //     { path: "/store/product/:productid", element: <ProductDisplay /> },
-  //   ],
-  // },
-]);
-
+import AuthProtect from "./Pages/AuthRouter/AuthProtect";
+import AuthLogin from "./Pages/AuthRouter/AuthLogin";
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Root />,
+      children: [
+        { index: true, element: <HomePage /> },
+        { path: "about", element: <AboutPage /> },
+        { path: "contact", element: <ContactPage /> },
+        {
+          element: <AuthLogin />,
+          children: [
+            { path: "signin", element: <SignIn /> },
+            { path: "signup", element: <SignUp /> },
+          ],
+        },
+        { path: "forgotPassword", element: <ForgotPassword /> },
+        {
+          element: <AuthProtect />,
+          children: [
+            {
+              path: "store",
+              id: "product-details",
+              loader: displayLoader,
+              children: [
+                { index: true, element: <Storepage /> },
+                { path: ":id", element: <ProductDisplay /> },
+              ],
+            },
+          ],
+        },
+      ],
+    },
+    // {
+    //   path: "/store",
+    //   element: <Storepage />,
+    //   children: [
+    //     { path: "/store/product/:productid", element: <ProductDisplay /> },
+    //   ],
+    // },
+  ]);
   return <RouterProvider router={router} />;
 }
-
 export default App;
