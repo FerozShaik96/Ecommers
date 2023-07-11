a5b525d99bd5ca6b660b6a53b973535007821556
FerozShaik96 <ferozshaik1997@gmail.com>
2023-07-11 12:26:48 +0530

    implemented Auto Logout  After 5 min

--- src/Store/AuthContect/auth-Context.js
+++ src/Store/AuthContect/auth-Context.js
@@ -8,6 +8,9 @@ const AuthContext = React.createContext({
 
 export const AuthContextProvider = (props) => {
   const intialToken = localStorage.getItem("Token");
+  setTimeout(() => {
+    localStorage.removeItem("Token");
+  }, 300000);
   const [token, setToken] = useState(intialToken);
   const userIsLoggedIn = !!token;
   const LoginHandler = (token) => {
