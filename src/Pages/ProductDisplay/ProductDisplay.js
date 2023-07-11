1e9094ecf74996971e9a4e463f12295d84bb7918
FerozShaik96 <ferozshaik1997@gmail.com>
2023-07-11 20:26:42 +0530

    Showed Specif product Details

--- src/Pages/ProductDisplay/ProductDisplay.js
+++ src/Pages/ProductDisplay/ProductDisplay.js
@@ -1,52 +1,33 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import { useParams } from "react-router-dom";
-const productsArr = [
-  {
-    id: 1,
-    title: "Colors",
-    price: 100,
-    imageUrl: "https://prasadyash2411.github.io/ecom-website/img/Album%201.png",
-  },
 
-  {
-    id: 2,
-    title: "Black and white Colors",
-    price: 50,
-    imageUrl: "https://prasadyash2411.github.io/ecom-website/img/Album%202.png",
-  },
-
-  {
-    id: 3,
-    title: "Yellow and Black Colors",
-    price: 70,
-    imageUrl: "https://prasadyash2411.github.io/ecom-website/img/Album%203.png",
-  },
-
-  {
-    id: 4,
-    title: "Blue Color",
-    price: 100,
-    imageUrl: "https://prasadyash2411.github.io/ecom-website/img/Album%204.png",
-  },
-];
 const ProductDisplay = () => {
   const { id } = useParams();
   const paramsId = +id;
-  const Data = productsArr.filter((item) => {
-    return item.id === paramsId;
-  });
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    fetch(`https://fakestoreapi.com/products`)
+      .then((res) => res.json())
+      .then((data) => setData(data))
+      .catch((error) => alert(error));
+    // console.log(data)
+  }, []);
+  const product = data.find((obj) => obj.id === paramsId);
+  console.log(product);
   return (
     <Container>
-      <Row className="mt-5 d-flex  justify-content-center">
-        <Col className=" d-flex  justify-content-center">
-          <img src={Data[0].imageUrl} alt={Data[0].title} />
-        </Col>
-        <Col className="flex-column align-items-center p-4 ">
-          <h3 className="display-4">{Data[0].title}</h3>
-          <h3>${Data[0].price}</h3>
-        </Col>
-      </Row>
+      {product && (
+        <Row className="mt-5 d-flex  justify-content-center">
+          <Col className=" d-flex  justify-content-center">
+            <img src={product.image} alt={product.title} className="w-50" />
+          </Col>
+          <Col className="flex-column align-items-center p-4 ">
+            <h3 className="display-4">{product.title}</h3>
+            <h3>${product.price}</h3>
+          </Col>
+        </Row>
+      )}
     </Container>
     // <Container>
     //   <Row className="mt-5 d-flex justify-content-center">
